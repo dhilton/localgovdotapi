af4c1d6e6b46787fcc981bcecefd680ed6b6eb0c
Dan Hilton <daniel.hilton@gmail.com>
2014-11-09 21:58:21 +0000

    Update validate script to raise sensbible error if flex not installed.

---
+++ validate.py
@@ -0,0 +1,17 @@
+import os
+try:
+    import flex
+except ImportError:
+    raise ImportError("""Validator can not find the python package Flex.
+    	 You need to install it, to be able to validate the 
+    	 swagger api specs. To install it use: pip install flex""")
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
+
